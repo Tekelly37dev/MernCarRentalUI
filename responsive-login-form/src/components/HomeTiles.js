1f83508007e586cf681f3adc2c478cd3f5fad28c
Tekelly37dev <75715317+Tekelly37dev@users.noreply.github.com>
2024-10-06 20:39:01 -0400

    Implemented image gallery - Home tab UI

---
+++ responsive-login-form/src/components/HomeTiles.js
@@ -0,0 +1,51 @@
+import "./hometiles.css"
+
+const HomeTiles = ()=>(
+
+        <div class="gallery">
+          <div class="columns is_mobile is_flex_wrap_wrap is_centered">
+              <div class="column is_half_mobile is_half_tablet k3t2f is_one_quarter_desktop">
+                <div class="xY_Tu">
+                  <a class="LDsBM Tcv1Y hqeWz bISpP UKqWW" href="/schedule-service/" target="_self">
+                    <span class="OVAFC bfSJK">
+                      <img width="60" height="60" loading="lazy" class="lazyload Ya17M NysAs" src="https://cdn.foxdealer.com/cdn-cgi/image/width=611,fit=contain,quality=85,format=auto/https://static.foxdealer.com/792/2024/03/schedule-service.jpg" srcset="https://cdn.foxdealer.com/cdn-cgi/image/width=320,fit=contain,quality=85,format=auto/https://static.foxdealer.com/792/2024/03/schedule-service.jpg 320w,https://cdn.foxdealer.com/cdn-cgi/image/width=611,fit=contain,quality=85,format=auto/https://static.foxdealer.com/792/2024/03/schedule-service.jpg 611w" sizes="(min-width: 611px) 611px,(min-width: 320px) 320px,100vw" alt="Schedule Service"/>
+                    </span>
+                    <span class="qIxnz bX7qB">Schedule Service</span>
+                  </a>
+                </div>
+                </div>
+              <div class="column is_half_mobile is_half_tablet k3t2f is_one_quarter_desktop">
+                <div class="xY_Tu">
+                  <a class="LDsBM Tcv1Y hqeWz bISpP UKqWW" href="/order-parts/" target="_self">
+                    <span class="OVAFC bfSJK">
+                      <img width="60" height="60" loading="lazy" class="lazyload Ya17M NysAs" src="https://cdn.foxdealer.com/cdn-cgi/image/width=611,fit=contain,quality=85,format=auto/https://static.foxdealer.com/792/2024/03/request-parts.jpg" srcset="https://cdn.foxdealer.com/cdn-cgi/image/width=320,fit=contain,quality=85,format=auto/https://static.foxdealer.com/792/2024/03/request-parts.jpg 320w,https://cdn.foxdealer.com/cdn-cgi/image/width=611,fit=contain,quality=85,format=auto/https://static.foxdealer.com/792/2024/03/request-parts.jpg 611w" sizes="(min-width: 611px) 611px,(min-width: 320px) 320px,100vw" alt="Request Parts"/>
+                    </span>
+                    <span class="qIxnz bX7qB">Request Parts</span>
+                  </a>
+                </div>
+              </div>
+              <div class="column is_half_mobile is_half_tablet k3t2f is_one_quarter_desktop">
+                <div class="xY_Tu">
+                  <a class="LDsBM Tcv1Y hqeWz bISpP UKqWW" href="/apply-for-financing/" target="_self">
+                    <span class="OVAFC bfSJK">
+                      <img width="60" height="60" loading="lazy" class="lazyload Ya17M NysAs" src="https://cdn.foxdealer.com/cdn-cgi/image/width=611,fit=contain,quality=85,format=auto/https://static.foxdealer.com/792/2024/03/financing.jpg" srcset="https://cdn.foxdealer.com/cdn-cgi/image/width=320,fit=contain,quality=85,format=auto/https://static.foxdealer.com/792/2024/03/financing.jpg 320w,https://cdn.foxdealer.com/cdn-cgi/image/width=611,fit=contain,quality=85,format=auto/https://static.foxdealer.com/792/2024/03/financing.jpg 611w" sizes="(min-width: 611px) 611px,(min-width: 320px) 320px,100vw" alt="Apply For Financing"/>
+                    </span>
+                    <span class="qIxnz bX7qB">Apply For Financing</span>
+                  </a>
+                </div>
+              </div>
+              <div class="column is_half_mobile is_half_tablet k3t2f is_one_quarter_desktop">
+                <div class="xY_Tu">
+                  <a class="LDsBM Tcv1Y hqeWz bISpP UKqWW" href="/inventory/new-specials/" target="_self">
+                    <span class="OVAFC bfSJK">
+                      <img width="60" height="60" loading="lazy" class="lazyload Ya17M NysAs" src="https://cdn.foxdealer.com/cdn-cgi/image/width=611,fit=contain,quality=85,format=auto/https://static.foxdealer.com/792/2024/03/new-specials.jpg" srcset="https://cdn.foxdealer.com/cdn-cgi/image/width=320,fit=contain,quality=85,format=auto/https://static.foxdealer.com/792/2024/03/new-specials.jpg 320w,https://cdn.foxdealer.com/cdn-cgi/image/width=611,fit=contain,quality=85,format=auto/https://static.foxdealer.com/792/2024/03/new-specials.jpg 611w" sizes="(min-width: 611px) 611px,(min-width: 320px) 320px,100vw" alt="New Car Specials"/>
+                    </span>
+                    <span class="qIxnz bX7qB">New Lease Specials</span>
+                  </a>
+                </div>
+              </div>
+          </div>
+        </div>
+      
+);
+export default HomeTiles
