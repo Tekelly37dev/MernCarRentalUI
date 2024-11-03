7335e9be88e630d6f5ad608db579435eeb1dd538
Tekelly37dev <75715317+Tekelly37dev@users.noreply.github.com>
2024-11-03 17:22:25 -0500

    Made tweaks to detail display

--- responsive-login-form/src/components/Accordion.js
+++ responsive-login-form/src/components/Accordion.js
@@ -74,8 +74,9 @@ const Accordion = () => {
                   columnGap: '100px',
                   rowGap: '8px',
                   listStyleType: 'disc',
-                  paddingLeft: '20px',
+                  paddingLeft: '120px',
                   textAlign: 'left',
+                  color: 'rgb(127, 127, 127)' 
                 }}
                 >
                   {item.content.map((listItem, i) => (
@@ -83,16 +84,7 @@ const Accordion = () => {
                   ))}
                 </ul>
               ) : item.isColumnList ? ( 
-                <div className='content-card'
-                style={{
-                  marginBottom: '2px',
-                  marginLeft:'20px',
-                  backgroundColor: '#f0f0f0', // light gray background
-                  width: '95%',
-                  height:'100%'
-                  
-                }}
-                >
+                <div className='content-card'>
                 <div
                 style={{
                   display: 'grid',
@@ -102,23 +94,42 @@ const Accordion = () => {
                   width: '100%',
                   // marginBottom: '25px',
                   marginLeft:'20px',
-                  marginTop:'20px',
+                  // marginTop:'20px',
                   textAlign: 'left',
+                  
                 }}
               >
-
                     {item.content.map((listItem, i) => (
                     <div key={i} style={{ margin: '8px 8px 8px 8px', display: 'flex', alignItems: 'center' }}>
                       {/* Check if an icon exists for this item and render it */}
-                      {featureIcons[listItem.toLowerCase()] || featureIcons["standard"]}
-                      <span style={{ marginLeft: '8px', }}>{listItem}</span>
+                      {/* Apply black color to the icon */}
+                      <span style={{ color: 'black' }}>
+                        {featureIcons[listItem.toLowerCase()] || featureIcons["standard"]}
+                      </span>
+                      {/* Apply grey color to the accompanying text */}
+                      <span style={{ marginLeft: '8px', color: 'rgb(127, 127, 127)' }}>{listItem}</span>
                     </div>
                   ))}
                 </div>
               </div>
-              )
-              
-              :(
+              ) : item.isList ?(
+                <ul
+                style={{
+                  display: 'grid',
+                  gridTemplateColumns: '1fr 1fr',
+                  columnGap: '100px',
+                  rowGap: '8px',
+                  listStyleType: 'none', // Remove bullets
+                  paddingLeft: '120px',
+                  textAlign: 'left',
+                  color: 'rgb(127, 127, 127)' 
+                }}
+              >
+                {item.content.map((listItem, i) => (
+                  <li key={i} style={{ marginBottom: '8px' }}>{listItem}</li>
+                ))}
+              </ul>
+            ):(
                 <p>{item.content}</p>
               )}
             </div>
@@ -172,12 +183,81 @@ const accordionData = [
   },
   {
     title: "Options",
-    content: "Accordion sections can expand or collapse to show or hide content, helping reduce visual clutter.",
+    content:[
+      "Super white",
+      "Power Steering",
+      "4-Wheel Disc Brakes",
+      "Wheel Covers",
+      "Tires - Front All-Season",
+      "Tires - Rear All-Season",
+      "Temporary Spare Tire",
+      "Rear Defrost",
+      "Variable Speed Intermittent Wipers",
+      "Automatic Headlights",
+      "Automatic Highbeams",
+      "MP3 Capability",
+      "Telematics",
+      "WiFi Hotspot",
+      "Requires Subscription",
+      "Bucket Seats",
+      "Adjustable Steering Wheel",
+      "Power Windows",
+      "Power Door Locks",
+      "Cruise Control",
+      "Woodgrain Interior Trim",
+      "Driver Vanity Mirror",
+      "Power Windows",
+      "Trip Computer",
+      "Traction Control",
+      "Traction Control",
+      "Lane Departure Warning",
+      "Lane Departure Warning",
+      "Driver Monitoring",
+      "Driver Air Bag",
+      "Front Head Air Bag",
+      "Passenger Air Bag Sensor",
+      "Rear Side Air Bag",
+      "Child Safety Locks",
+      "Front Wheel Drive",
+      "ABS",
+      "Brake Assist",
+      "Steel Wheels",
+      "Tires - Front Performance",
+      "Tires - Rear Performance",
+      "Power Mirror(s)",
+      "Intermittent Wipers",
+      "Daytime Running Lights",
+      "LED Headlights",
+      "AM/FM Stereo",
+      "Bluetooth Connection",
+      "Auxiliary Audio Input",
+      "Smart Device Integration",
+      "Steering Wheel Audio Controls",
+      "Rear Bench Seat",
+      "Trip Computer",
+      "Keyless Entry",
+      "Remote Trunk Release",
+      "Adaptive Cruise Control",
+      "Cloth Seats",
+      "Passenger Vanity Mirror",
+      "Power Door Locks",
+      "Immobilizer",
+      "Stability Control",
+      "Front Side Air Bag",
+      "Lane Keeping Assist",
+      "Front Collision Mitigation",
+      "Tire Pressure Monitor",
+      "Passenger Air Bag",
+      "Rear Head Air Bag",
+      "Front Side Air Bag",
+      "Knee Air Bag",
+      "Back-Up Camera"],
+    isList: true
    
   },
   {
     title: "Description",
-    content: "It allows users to consume content in small chunks, making it easier to navigate and more user-friendly.",
+    content: "Recent Arrival! 2019 Toyota Camry SE KBB Fair Market Range High: $15,002 ABS brakes, Active Cruise Control, Alloy wheels, Electronic Stability Control, Illuminated entry, Low tire pressure warning, Navigation System, Remote keyless entry, Traction control. CARFAX One-Owner. THIS VEHICLE INCLUDES THE FOLLOWING FEATURES: ABS brakes, Active Cruise Control, Alloy wheels, Electronic Stability Control, Illuminated entry, Low tire pressure warning, Navigation System, Remote keyless entry, Traction control, 4-Wheel Disc Brakes, 6 Speakers, Air Conditioning, AM/FM radio, Apple CarPlay, Auto High-beam Headlights, Automatic temperature control, Brake assist, Bumpers: body-color, Delay-off headlights, Driver door bin, Driver vanity mirror, Dual front impact airbags, Dual front side impact airbags, Emergency communication system: Safety Connect, Exterior Parking Camera Rear, Fabric Seat Trim w/SofTex, Four wheel independent suspension, Front anti-roll bar, Front Bucket Seats, Front Bucket Sport Seats, Front Center Armrest, Front reading lights, Fully automatic headlights, Knee airbag, Leather Shift Knob, Occupant sensing airbag, Outside temperature display, Overhead airbag, Overhead console, Panic alarm, Passenger door bin, Passenger vanity mirror, Power door mirrors, Power driver seat, Power steering, Power windows, Radio data system, Radio: Entune 3.0 Audio w/App Suite, Rear anti-roll bar, Rear reading lights, Rear seat center armrest, Rear side impact airbag, Rear window defroster, Security system, Speed control, Speed-sensing steering, Split folding rear seat, Spoiler, Sport steering wheel, Steering wheel mounted audio controls, Tachometer, Telescoping steering wheel, Tilt steering wheel, Trip computer, Variably intermittent wipers, and Wheels: 8.0J x 18\" Black Machined-Finish Alloy. Awards: * 2019 KBB.com 10 Most Comfortable Cars Under $30,000 * 2019 KBB.com 10 Best Sedans Under $30,000 * 2019 KBB.com Brand Image Awards * 2019 KBB.com Best Resale Value Awards Call us at 803-786-4111 or come by and see why the Midlands Toyota Red Carpet Experience is the best in town, a World Class experience from beginning to end."
    
   }
   
